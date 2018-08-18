8d997bad3163082dc3836b507fbc8ccd80460688
Moritz Hoffmann <moritz1@gmx.com>
2018-08-18 03:35:13 +0200

    Add MQ

--- routes/dataset.js
+++ routes/dataset.js
@@ -1,9 +1,24 @@
 const Router    = require('koa-router');
+const Amqp      = require('amqplib/callback_api');
+const Config    = require('config');
 
 const model = {
 	Dataset: require('../models/dataset').model,
 	Analysis: require('../models/analysis').model,
 };
+
+// parse config
+const config       = Config.get('server');
+
+let msgChannel;
+const q = 'classify_queue';
+
+Amqp.connect(config.ampq.url, (err, conn) => {
+	conn.createChannel((err2, ch) => {
+		msgChannel = ch;
+		ch.assertQueue(q, {durable: true});
+	});
+});
 
 // mounted at /dataset
 const datasetRouter = new Router();
@@ -16,6 +31,11 @@ datasetRouter.post('/submit', async (ctx) => {
 		dataset: dataset._id,
 	});
 	console.log(`New Dataset: ${dataset._id.toString()} \nNew Analysis: ${analysis._id.toString()}`);
+	const msg = {
+		action: 'analyze',
+		analysis_id: analysis._id.toHexString(),
+	};
+	msgChannel.sendToQueue(q, Buffer.from(JSON.stringify(msg)), {persistent: true});
 });
 
 module.exports = datasetRouter;
