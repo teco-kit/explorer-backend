60a1e8dbd7c06b7b5c707b9324db5b2916e879a8
Tobias King <tk.king.tk@gmail.com>
2021-02-13 12:06:40 +0100

    Made project unique for a user

--- controller/projects.js
+++ controller/projects.js
@@ -1,6 +1,5 @@
-const project = require("../models/project");
-const Project = require("../models/project").model;
-const UserModel = require("../models/user").model;
+const Project = require('../models/project').model;
+const UserModel = require('../models/user').model;
 
 function filterProjectNonAdmin(ctx, project) {
   const { authId } = ctx.state;
@@ -17,7 +16,7 @@ async function getProjects(ctx, next) {
   const body = await Project.find({
     $or: [{ admin: authId }, { users: authId }],
   });
-  ctx.body = body.map((elm) => filterProjectNonAdmin(ctx, elm));
+  ctx.body = body.map(elm => filterProjectNonAdmin(ctx, elm));
   ctx.status = 200;
   return ctx;
 }
@@ -25,23 +24,34 @@ async function getProjects(ctx, next) {
  * Creates a new project
  */
 async function createProject(ctx) {
-  const project = ctx.request.body;
-  // The admin is the one creating the project
-  const { authId } = ctx.state;
-  project.admin = authId;
-  const document = new Project(project);
-  await document.save();
+  try {
+    console.log("Creating project")
+    const project = ctx.request.body;
+    // The admin is the one creating the project
+    const { authId } = ctx.state;
+    project.admin = authId;
+    const document = new Project(project);
+    await document.save();
 
-  ctx.body = document;
-  ctx.status = 201;
-  return ctx;
+    ctx.body = document;
+    ctx.status = 201;
+    return ctx;
+  } catch (e) {
+    console.log(e);
+    console.log("Error")
+    if (e.code === 11000 && e.keyPattern.admin && e.keyPattern.name) {
+      ctx.body = { error: 'A project with this name already exists' };
+      ctx.status = 400;
+    }
+    return ctx;
+  }
 }
 
 /*
  * Deletes a project when a user has the access rights
  */
 async function deleteProjectById(ctx) {
-  const authId = ctx.state.authId;
+  const { authId } = ctx.state;
   await Project.findOneAndDelete({
     $and: [
       { _id: ctx.params.id },
@@ -54,17 +64,23 @@ async function deleteProjectById(ctx) {
 }
 
 async function updateProjectById(ctx) {
-  const { authId } = ctx.state;
-  const project = ctx.request.body;
-  project.users = ctx.request.body.users.map((elm) => {
-    return typeof elm === "object" ? elm._id : elm;
-  });
-  await Project.findOneAndUpdate(
-    { $and: [{ _id: ctx.params.id }, { admin: authId }] },
-    { $set: project }
-  );
-  ctx.body = { message: `updated project with id: ${ctx.params.id}` };
-  ctx.status = 200;
+  try {
+    const { authId } = ctx.state;
+    const project = ctx.request.body;
+    project.users = ctx.request.body.users.map(elm => (typeof elm === 'object' ? elm._id : elm));
+    await Project.findOneAndUpdate(
+      { $and: [{ _id: ctx.params.id }, { admin: authId }] },
+      { $set: project }
+    );
+    ctx.body = { message: `updated project with id: ${ctx.params.id}` };
+    ctx.status = 200;
+  } catch (e) {
+    if (e.code === 11000 && e.keyPattern.admin && e.keyPattern.name) {
+      ctx.body = { error: 'A project with this name already exists' };
+      ctx.status = 400;
+      return ctx;
+    }
+  }
   return ctx;
 }
 
